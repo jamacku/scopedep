17ecf25b9c80081e1979913fb1b6c2aacff06e90
Jan Macku <jamacku@redhat.com>
2023-05-16 14:54:30 +0200

    reproducer for rhbz#2009307

--- main.go
+++ main.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"log"
-	"os"
 	"os/exec"
-	"os/signal"
-	"syscall"
 
 	"github.com/coreos/go-systemd/v22/dbus"
 )
@@ -24,7 +21,7 @@ func main() {
 	defer sd.Close()
 
 	// Spawn "worker" process
-	cmd := exec.Command("sleep", "infinity")
+	cmd := exec.Command("sleep", "1")
 	err = cmd.Start()
 	if err != nil {
 		log.Fatalf("failed to start worker: %v\n", err)
@@ -35,7 +32,11 @@ func main() {
 	// Wrap a scope unit around child process and make sure add Before dependecy on the main unit
 	// We are adding Before dependecy because want deterministic shutdown order
 	resultChan := make(chan string)
-	scopeProperties := []dbus.Property{dbus.PropBefore(UNIT), dbus.PropSlice("system.slice"), dbus.PropPids(uint32(cmd.Process.Pid))}
+	scopeProperties := []dbus.Property{dbus.PropSlice("system.slice"), dbus.PropPids(uint32(cmd.Process.Pid))}
+
+	go func() {
+		cmd.Wait();
+	}()
 
 	_, err = sd.StartTransientUnit(SCOPE, "replace", scopeProperties, resultChan)
 	if err != nil {
@@ -48,37 +49,4 @@ func main() {
 	}
 
 	log.Printf("%s started\n", SCOPE)
-
-	// Setup signal handling
-	signalChan := make(chan os.Signal, 1)
-	signal.Ignore()
-	signal.Notify(signalChan, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP)
-
-	for {
-		sig := <-signalChan
-
-		if sig == syscall.SIGTERM || sig == syscall.SIGINT || sig == syscall.SIGHUP {
-			// We want to reuse the channel so it must be empty before we do that
-			if len(resultChan) != 0 {
-				panic("unexpected message in the channel")
-			}
-
-			log.Printf("about to stop %s\n", SCOPE)
-
-			// Kill the worker process and wait for it
-			// Note that calling StopUnit() DBus API with "replace" job mode would create deadlock
-			err = cmd.Process.Kill()
-			if err != nil {
-				log.Fatalf("failed to kill worker process: %v\n", err)
-			}
-
-			cmd.Wait()
-
-			log.Printf("worker killed\n")
-
-			break
-		}
-	}
-
-	log.Printf("%s exiting\n", UNIT)
 }
